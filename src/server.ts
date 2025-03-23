b428ccbb3a4da1123b3336e7abd221599eeb74a9
eyluccas <eyluccas@gmail.com>
2025-03-22 23:42:56 -0300

    dependencies prisma studio orm database, file .env

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ const HOST = "localhost";
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! Wiht Typescript\n");
 });
-
+app.get("/instance", (req: Request, res: Response) => { 
+  res.send('Create a new instance of')
+})
 app.listen(PORT, HOST, () => {
   console.log(`Local Server Startup | http://${HOST}:${PORT}`);
 });
