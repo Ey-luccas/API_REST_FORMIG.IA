24605d514cf32b5afccd2850bc217d45d248c27a
eyluccas <eyluccas@gmail.com>
2025-03-22 23:07:49 -0300

    install dependecies: Typescript, Nodemon, Mysql2, express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-
+const PORT = 3000
+const HOST = "localhost";
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!\n");
 });
 
-app.listen(3000, "127.0.0.1", () => {
-  console.log("Listening on https://localhost:127.0.0.1:3000");
+app.listen(PORT, HOST, () => {
+  console.log(`Local Server Startup | http://${HOST}:${PORT}`);
 });
